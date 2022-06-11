13c5d5df958533d92e552121a16a539d4069e290
gjhonic <gjhonic@gmail.com>
2022-06-11 21:13:39 +0800

    структура проекта

---
+++ frontend/assets/js/vue.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      config: {
+          projectName: 'Tasks',
+          version: '0.1 beta',
+          author: 'gjhonic',
+      }
+    }
+  })
