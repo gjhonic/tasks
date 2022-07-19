cdb05f4394d33112a104dc3b2f56dd6cbcf0e282
DiGjhonic <45502448+gjhonic@users.noreply.github.com>
2022-07-19 19:25:19 +0800

    Рефакторинг

---
+++ assets/js/vue/index.js
@@ -0,0 +1,11 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        config: {
+            projectName: 'Tasks',
+            version: '0.1',
+            lang: 'en',
+            author: 'gjhonic',
+        },
+    },
+});
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once 'core/libs/functions.php';
+
+$routes = [
+    '' => 'index',
+    'index' => 'index',
+    'workspace' => 'workspace',
+];
+
+$query = substr($_SERVER['REDIRECT_URL'], 1);
+
+if(strripos($query, 'backend/actions/') !== false) {
+    require_once $query;
+} else {
+    if(isset($routes[$query])) {
+        require_once 'controllers/' . $routes[$query] . '.php';
+    } else {
+        require_once 'core/pages/404.php';
+    }
+}
+
